9b6fbf868ee65416c1fe04a6ae6185c70d6735a5
Michael Lachand <lachand_michael@hotmail.com>
2024-06-19 17:40:47 +0200

    Installation Debug

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
     public function homepage(): Response
     {
-        return new Response('this is homepage');
+        return $this->render('main/homepage.html.twig');
     }
 }

7d7ad4064923e15db8534e388827c72040a05759
Michael Lachand <lachand_michael@hotmail.com>
2024-06-20 16:16:00 +0200

    Creation Enum

--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -4,14 +4,14 @@ namespace App\Model;
 
 class Starship
 {
-        public function __construct(
-            private int $id,
-            private string $name,
-            private string $class,
-            private string $captain,
-            private string $status,
-        ) {
-        }
+    public function __construct(
+        private int $id,
+        private string $name,
+        private string $class,
+        private string $captain,
+        private StarshipStatusEnum $status,
+    ) {
+    }
 
     public function getId(): int
     {
@@ -33,7 +33,7 @@ class Starship
         return $this->captain;
     }
 
-    public function getStatus(): string
+    public function getStatus(): StarshipStatusEnum
     {
         return $this->status;
     }

ae955fdd96d1663a580236ab64891225afb4d3b5
Michael Lachand <lachand_michael@hotmail.com>
2024-06-20 11:28:14 +0200

    Creation service

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
-    public function homepage(): Response
+    public function homepage(StarshipRepository $repository): Response
     {
-        return $this->render('main/homepage.html.twig');
+        $ships = $repository->findAll();
+        $myShip = $ships[array_rand($ships)];
+
+
+        return $this->render('main/homepage.html.twig', [
+            'myShip' => $myShip,
+            'ships' => $ships,
+        ]);
     }
 }

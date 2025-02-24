85aa97605b77708a0b9e26bb27e1001933a836e9
Michael Lachand <lachand_michael@hotmail.com>
2025-02-24 11:38:18 +0100

    Add Twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
     public function homepage(): Response
     {
-        return new Response('Mike le boss');
+        $starshipCount = 457;
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
     }
 }

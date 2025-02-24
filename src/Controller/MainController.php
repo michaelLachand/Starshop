9aa5daa048ff2892a946079d32980c6f047cd668
Michael Lachand <lachand_michael@hotmail.com>
2025-02-24 15:33:05 +0100

    Add Starship Repository

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,25 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'main')]
-    public function homepage(): Response
+    #[Route('/')]
+    public function homepage(StarshipRepository $starshipRepository): Response
     {
-        $starshipCount = 457;
-        $myShip = [
-            'name' => 'USS LeafyCruiser (NCC-0001)',
-            'class' => 'Garden',
-            'captain' => 'Jean-Luc Pickles',
-            'status' => 'under construction',
-        ];
+        $ships = $starshipRepository->findAll();
+        $myShip = $ships[array_rand($ships)];
+
         return $this->render('main/homepage.html.twig', [
-            'numberOfStarships' => $starshipCount,
             'myShip' => $myShip,
+            'ships' => $ships,
         ]);
     }
 }
--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Model\Starship;
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,31 +10,9 @@ use Symfony\Component\Routing\Attribute\Route;
 class StarshipApiController extends AbstractController
 {
     #[Route('/api/starships')]
-    public function getCollection(): Response
+    public function getCollection(StarshipRepository $starshipRepository): Response
     {
-        $starships = [
-            new Starship(
-                1,
-                'USS LeafyCruiser (NCC-0001)',
-                'Garden',
-                'Jean-Luc Pickles',
-                'taken over by Q'
-            ),
-            new Starship(
-                2,
-                'USS Espresso (NCC-1234-C)',
-                'Latte',
-                'James T. Quick!',
-                'repaired',
-            ),
-            new Starship(
-                3,
-                'USS Wanderlust (NCC-2024-W)',
-                'Delta Tourist',
-                'Kathryn Journeyway',
-                'under construction',
-            ),
-        ];
+        $starships = $starshipRepository->findAll();
 
         return $this->json($starships);
     }

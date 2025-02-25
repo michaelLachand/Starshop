a517ead753006ee9ccba5f075927f1dbf8695dc4
Michael Lachand <lachand_michael@hotmail.com>
2025-02-25 15:56:30 +0100

    Add pagination with pagerfanta

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Starship;
 use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -13,10 +14,13 @@ class MainController extends AbstractController
     #[Route('/')]
     public function homepage(
         StarshipRepository $repository,
+        Request $request,
     ): Response
     {
 
         $ships = $repository->findIncomplete();
+        $ships->setMaxPerPage(5);
+        $ships->setCurrentPage($request->query->get('page', 1));
 
         $myShip = $repository->findMyShip();
 

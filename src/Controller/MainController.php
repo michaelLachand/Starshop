a7f682a5cb468a3821e443e7bee4f2a6b90ce28a
Michael Lachand <lachand_michael@hotmail.com>
2025-02-25 10:32:36 +0100

    Add cache

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,8 @@ use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MainController extends AbstractController
@@ -14,14 +16,19 @@ class MainController extends AbstractController
     public function homepage(
         StarshipRepository $starshipRepository,
         HttpClientInterface $client,
+        CacheInterface $issLocationPool,
     ): Response
     {
+
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
 
-        $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
-        $issData = $response->toArray();
-        dump($issData);
+        $issData = $issLocationPool->get('iss_location_data', function () use ($client): array {
+
+            $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
+            return $response->toArray();
+        });
+
         return $this->render('main/homepage.html.twig', [
             'myShip' => $myShip,
             'ships' => $ships,
